8377a32e3675396e50d25f95c44078efd2c6a061
1978393699 <100391777+1978393699@users.noreply.github.com>
2022-10-29 16:29:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '仲崇宇',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNALq55aQLFGJ8VDm7sEzb2mw9iU',
+      id: 'oNALq58koOegA_NW_Z5uzbw6DTlw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'CJcctTezDuBJNbKcxKczIRVXv6QLSEosKFnQ0RIblv0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
