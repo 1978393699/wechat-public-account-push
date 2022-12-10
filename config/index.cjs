a675297c0d76ee85c353e35903c652a87b9b5c65
1978393699 <100391777+1978393699@users.noreply.github.com>
2022-12-10 16:55:50 +0800

    Create index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,13 +45,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'CJcctTezDuBJNbKcxKczIRVXv6QLSEosKFnQ0RIblv0',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '代虎',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNALq55aQLFGJ8VDm7sEzb2mw9iU',
+      id: '',
     }
   ],
 
